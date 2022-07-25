97ef0d5d78a91983ec282cb908f08bf0ed840f08
zhangzhi <1523334128@qq.com>
2022-07-25 17:38:22 +0800

    study(Spring): autowire
    自动装配的方式
     ·按类型（常用）
     ·按名称
     ·按构造方法
     ·不启用自动装配
    自动装配只需要修改applicationContext.ml配置文件即可：
     (1)将<property>标签删除
     (2)在<bean>标签中添加autowire属性

---
+++ spring_07_di_autoware/src/main/java/com/example/dao/impl/BookDaoImpl.java
@@ -0,0 +1,9 @@
+package com.example.dao.impl;
+
+import com.example.dao.BookDao;
+
+public class BookDaoImpl implements BookDao {
+    public void save() {
+        System.out.println("book dao save ...");
+    }
+}
