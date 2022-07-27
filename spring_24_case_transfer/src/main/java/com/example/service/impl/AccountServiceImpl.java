441bf0d8521db207ca6ea6dd35161cb0a750b2c0
zhangzhi <1523334128@qq.com>
2022-07-27 16:24:08 +0800

    study(Spring): AOP事务管理
    1.Spring事务作用：在数据层或业务层保障一系列的数据库操作同成功同失败
    2.Spring为了管理事务，提供了一个平台事务管理器PlatformTransactionManager
    3.@Transactional可以写在接口类上、接口方法上、实现类上和实现类方法上
     · 写在接口类上，该接口的所有实现类的所有方法都会有事务
     · 写在接口方法上，该接口的所有实现类的该方法都会有事务
     · 写在实现类上，该类中的所有方法都会有事务
     · 写在实现类方法上，该方法上有事务
     · 建议写在实现类或实现类的方法上
    4.@EnableTransactionManagement：设置当前Spring环境中开启注解式事务支持

---
+++ spring_24_case_transfer/src/main/java/com/example/service/impl/AccountServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.service.impl;
+
+import com.example.dao.AccountDao;
+import com.example.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+
+    public void transfer(String out,String in ,Double money) {
+        accountDao.outMoney(out,money);
+        int i = 1/0;
+        accountDao.inMoney(in,money);
+    }
+
+}
